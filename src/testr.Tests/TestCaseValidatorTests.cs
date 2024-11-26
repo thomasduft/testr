53b54888bceca40e4e4fa962b63115f34f78568e
Thomas Duft <thomas.duft@buhlergroup.com>
2024-11-26 08:53:06 +0100

    Add validation test for login steps and corresponding test data

--- src/testr.Tests/TestCaseValidatorTests.cs
+++ src/testr.Tests/TestCaseValidatorTests.cs
@@ -20,4 +20,23 @@ public class TestCaseValidatorTests
     Assert.NotNull(result);
     Assert.True(result.IsValid);
   }
+
+  [Fact]
+  public async Task ValidateSteps_WithWrongTestData_ShouldReturnOneValidationErrors()
+  {
+    // Arrange
+    var file = Path.Combine(Environment.CurrentDirectory, "TestData", "TC-003-Login-Validation-Errors.md");
+    var testCase = await TestCase.FromTestCaseFileAsync(file, default);
+
+    var validator = new TestCaseValidator(testCase);
+
+    // Act
+    var result = validator.ValidateSteps(testCase.Steps);
+
+    // Assert
+    Assert.NotNull(result);
+    Assert.False(result.IsValid);
+    Assert.True(result.Errors.Count() == 1);
+    Assert.Equal("Requested value 'GetRole' was not found.", result.Errors.First().ErrorMessage);
+  }
 }
