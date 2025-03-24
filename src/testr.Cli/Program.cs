eb66936b3ae51bb9f352b499eacfbb9746177ded
Thomas Duft <thomas.duft@abraxas.ch>
2025-03-24 09:19:52 +0100

    feat: add install-playwright command and update launch configuration

--- src/testr.Cli/Program.cs
+++ src/testr.Cli/Program.cs
@@ -7,6 +7,7 @@ var services = new ServiceCollection()
     .AddCliCommand<TestCaseCommand>()
     .AddCliCommand<ValidateCommand>()
     .AddCliCommand<ManCommand>()
+    .AddCliCommand<InstallPlaywrightCommand>()
     .AddSingleton<Cli>();
 
 var provider = services.BuildServiceProvider();
